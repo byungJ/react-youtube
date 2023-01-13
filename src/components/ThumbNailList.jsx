36b26fed6c2afc6fba0384fd8fbaa1b2b311a436
qudwns6122 <qudwns6122@naver.com>
2023-01-13 10:09:02 +0900

    project structure

---
+++ src/components/ThumbNailList.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+
+export default function ThumbNailList() {
+
+    const [thumbList, setThumbList] = useState();
+    useEffect()
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
